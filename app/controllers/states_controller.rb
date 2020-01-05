0e5f472a15f964de6d278c0964b10771bf2e0454
ITSONLYJENNI <jennifer.f45@yahoo.com>
2020-01-05 09:44:37 +0000

    adding the box and navbar

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,4 +2,19 @@ class StatesController < ApplicationController
   def index
     @states = State.all
   end
+
+  def new
+    @states  = State.new
+  end
+
+  def create
+    State.create(state_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def state_params
+    params.require(:state).permit(:name, :description)
+  end
 end
